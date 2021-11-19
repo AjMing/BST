3bbe15fe07598a60b2e590e61973bb63dd153dab
AjMing <62439174+AjMing@users.noreply.github.com>
2021-11-19 03:56:09 +0000

    Initial commit

---
+++ BST.h
@@ -0,0 +1,85 @@
+// An integer binary search tree
+
+struct TreeNode {                                           
+   TreeNode *leftPtr; // pointer to left subtree 
+   int data; // node value                               
+   TreeNode *rightPtr; // pointer to right subtree
+}; // end structure treeNode 
+        
+typedef TreeNode * TreeNodePtr; // synonym for TreeNode*
+                    
+typedef struct {
+	int size;
+	TreeNodePtr root;
+}BST;
+
+// prototypes
+void insertNode( BST &, int );
+
+void insertNode_R(TreeNodePtr & t,int value){
+  if(t==NULL){
+   t=new TreeNode;
+	 if(t){
+		t->leftPtr=NULL;
+		t->rightPtr=NULL;
+		t->data=value;
+   }
+  }
+  else{
+     if(t->data>=value) /* left*/
+        insertNode_R(t->leftPtr,value);
+     else /*right*/
+        insertNode_R(t->rightPtr,value);
+   }
+}
+
+void insertNode( BST &b, int value ){
+	 TreeNodePtr t =b.root,new_node;
+	 int inserted=0;
+	new_node =new TreeNode;
+	if(new_node){
+		new_node->leftPtr=NULL;
+		new_node->rightPtr=NULL;
+		new_node->data=value;
+		/*First Node*/
+		if(!b.root)	b.root=new_node;
+		else {
+while(!inserted){
+   if(t->data >=value){
+     /* move/insert to the left*/
+     if(t->leftPtr!=NULL) t=t->leftPtr;
+     else{ 
+         t->leftPtr=new_node;
+         inserted++;
+     }
+  	 
+  }
+   else{
+	      /* move/ insert to the right*/
+    if(t->rightPtr) t=t->rightPtr;
+    else{
+  	 t->rightPtr=new_node;
+     inserted++;
+    }
+	}
+   
+  }//end while		
+  }//end else;
+  b.size++;
+  }
+  
+}//end function
+
+
+void inOrder( TreeNodePtr treePtr )
+{ 
+   // if tree is not empty, then traverse
+   if ( treePtr != NULL ) {        
+        
+      inOrder( treePtr->leftPtr ); //Recursion to the left
+ 
+      printf( "%3d", treePtr->data );  //print the value 
+   
+      inOrder( treePtr->rightPtr ); //Recursion to the right
+   } // end if                          
+} // end 
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+#include "BST.h"
+int main( int argc, char** argv )
+{ 
+   unsigned int i; // counter to loop from 1-10
+   int item; // variable to hold random values
+   BST b;
+
+   
+	b.root= NULL;  b.size=0;// tree initially empty
+for ( i = 1; i < argc; ++i ) { 
+     //  insertNode( b, atoi(argv[i]));
+     insertNode_R(b.root,atoi(argv[i]) );
+  } // end for
+   // traverse the tree preOrder
+   /*cout<< "\n\nThe preOrder traversal is:<<endl" ;
+   preOrder( rootPtr );*/
+
+   // traverse the tree inOrder
+    cout<<"\nThe inOrder traversal is:" <<endl;
+   inOrder( b.root );
+/*
+   // traverse the tree postOrder
+    cout<< "\n\nThe postOrder traversal is:" <<endl;
+   postOrder( rootPtr );
+*/
+} // end main
+
